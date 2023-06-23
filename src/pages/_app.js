58252a1df642496f22f5f5ec83a9cf554afcf5d3
RubasznyChomik <poreba.mateusz@gmail.com>
2023-06-23 15:10:11 +0200

    ended my form

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import BasicForm from './components/basicForm';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+function App() {
+  return (
+    <div className="app">
+      <BasicForm />
+    </div>
+  );
 }
+
+export default App;
